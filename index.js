13d106bf9c36d330e2dfedd188e40cefa844261e
Luca Nils Schmid <me@lucaschmid.net>
2016-11-16 18:52:32 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const requestDebug = require('request-debug')
+const { requestDebugger, nockGenerator, generateNockInstance } = require('./lib')
+const fs = require('fs')
+
+module.exports = function generateNocksFromRequests (
+    request,
+    { repeat = 999, delay = 200, output = 'mocks.js', flushOnExit = true } = {}
+  ) {
+  const cache = {}
+  const nockInstances = {}
+  let buffer = ''
+  requestDebug(
+    request,
+    requestDebugger(
+      cache,
+      nockInstances,
+      nockGenerator(delay, repeat),
+      s => { buffer += s }
+    )
+  )
+  const flush = () => {
+    const definitions =
+      Object.values(nockInstances)
+        .map((k, i) => generateNockInstance(k, nockInstances[k]))
+        .join('')
+    fs.writeFileSync(output, `${definitions}\n${buffer}`)
+  }
+
+  if (flushOnExit) {
+    process.on('exit', flush)
+    process.on('SIGTERM', flush)
+  }
+
+  return flush
+}
